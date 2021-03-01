39ec36a168f4e17fbfe25dbc521e5614ee581c79
Abdul Hadi <abdul.abbas.externo@ageas.pt>
2021-03-01 20:55:15 +0000

    search console by name and rating greater then val

--- session-three/dist/index.dev.js
+++ session-three/dist/index.dev.js
@@ -7,17 +7,30 @@ var axios = require("axios");
 var ELASTIC_URL = "http://localhost:9200";
 var INDEX = "books";
 
-var callDslSearchByName = function callDslSearchByName(name) {
+var callDslSearchRatingGte = function callDslSearchRatingGte(title, value) {
   var body, _ref, data;
 
-  return regeneratorRuntime.async(function callDslSearchByName$(_context) {
+  return regeneratorRuntime.async(function callDslSearchRatingGte$(_context) {
     while (1) {
       switch (_context.prev = _context.next) {
         case 0:
           body = {
             "query": {
-              "match": {
-                "title": name
+              "bool": {
+                "must": [{
+                  "match": {
+                    "title": title
+                  }
+                }],
+                "filter": [{
+                  "range": {
+                    "rating": {
+                      "gte": value
+                    }
+                  }
+                }],
+                "should": [],
+                "must_not": []
               }
             }
           };
@@ -37,11 +50,42 @@ var callDslSearchByName = function callDslSearchByName(name) {
   });
 };
 
+var callDslSearchByName = function callDslSearchByName(name) {
+  var body, _ref2, data;
+
+  return regeneratorRuntime.async(function callDslSearchByName$(_context2) {
+    while (1) {
+      switch (_context2.prev = _context2.next) {
+        case 0:
+          body = {
+            "query": {
+              "match": {
+                "title": name
+              }
+            }
+          };
+          _context2.next = 3;
+          return regeneratorRuntime.awrap(axios.post("".concat(ELASTIC_URL, "/").concat(INDEX, "/_search"), body));
+
+        case 3:
+          _ref2 = _context2.sent;
+          data = _ref2.data;
+          parseResult(data.hits.hits);
+
+        case 6:
+        case "end":
+          return _context2.stop();
+      }
+    }
+  });
+};
+
 var mapperBook = function mapperBook(hit) {
   return {
     title: hit._source.title,
     author: hit._source.author,
     price: hit._source.price,
+    rating: hit._source.rating,
     score: hit._score
   };
 };
@@ -55,23 +99,27 @@ var parseResult = function parseResult(hits) {
 };
 
 (function _callee() {
-  return regeneratorRuntime.async(function _callee$(_context2) {
+  return regeneratorRuntime.async(function _callee$(_context3) {
     while (1) {
-      switch (_context2.prev = _context2.next) {
+      switch (_context3.prev = _context3.next) {
         case 0:
           inquirer.prompt([{
             type: "input",
             name: "title",
             message: "What is the book's name: "
+          }, {
+            type: "number",
+            name: "rating",
+            message: "Rating greater then value: "
           }]).then(function (answers) {
-            callDslSearchByName(answers['title']);
+            callDslSearchRatingGte(answers['title'], answers['rating']); //callDslSearchByName(answers['title']);
           })["catch"](function (err) {
             return console.log(err);
           });
 
         case 1:
         case "end":
-          return _context2.stop();
+          return _context3.stop();
       }
     }
   });
--- session-three/index.js
+++ session-three/index.js
@@ -4,6 +4,38 @@ const axios = require("axios");
 const ELASTIC_URL = "http://localhost:9200";
 const INDEX = "books";
 
+
+const callDslSearchRatingGte = async (title, value) => {
+  const body = {
+    "query": {
+      "bool": {
+        "must": [
+          {
+            "match": {
+              "title": title
+            }
+          }
+        ],
+        "filter": [
+          {
+            "range": {
+              "rating": {
+                "gte": value
+              }
+            }
+          }
+        ],
+        "should": [],
+        "must_not": []
+      }
+    }
+  };
+
+  const { data } = await axios.post(`${ELASTIC_URL}/${INDEX}/_search`, body);
+
+  parseResult(data.hits.hits);
+};
+
 const callDslSearchByName = async (name) => {
 
   const body = {
@@ -24,6 +56,7 @@ const mapperBook = (hit) => {
     title: hit._source.title,
     author: hit._source.author,
     price: hit._source.price,
+    rating: hit._source.rating,
     score: hit._score
   };
 };
@@ -47,8 +80,14 @@ const parseResult = (hits) => {
       type: "input",
       name: "title",
       message: "What is the book's name: "
-    }
+    },
+    {
+      type: "number",
+      name: "rating",
+      message: "Rating greater then value: "
+    },
   ]).then((answers) => {
-    callDslSearchByName(answers['title']);
+    callDslSearchRatingGte(answers['title'], answers['rating']);
+    //callDslSearchByName(answers['title']);
   }).catch((err) => console.log(err));
 })();
